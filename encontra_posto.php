9ca96fd8491655a8fc921b49eb8a3ac5cf90a9e9
anderssonn2-hub <anderssonn2@gmail.com>
2026-02-26 19:47:05 +0000

    fix: correção erro ao processar em encontra

--- encontra_posto.php
+++ encontra_posto.php
@@ -21,6 +21,15 @@ function normalizarDataEntrada($s) {
     return '';
 }
 
+function normalizarDataIso($s) {
+    $s = trim((string)$s);
+    if ($s === '') return '';
+    if (preg_match('/^(\d{4})\-(\d{2})\-(\d{2})$/', $s, $m)) {
+        return $m[1] . '-' . $m[2] . '-' . $m[3];
+    }
+    return '';
+}
+
 function parseDatasAlvo($raw) {
     $out = array();
     $partes = preg_split('/[;,\s]+/', (string)$raw);
